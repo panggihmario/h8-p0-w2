b6f549e3c5100dffa09403d5ac42e8578d24c9e9
mario <panggihmario@gmail.com>
2018-03-27 09:50:20 +0700

    baru

--- exercise13.js
+++ exercise13.js
@@ -1,11 +1,18 @@
 function xo(str) {
-  var x = str.match(/x/g).length;
-  var o = str.match(/o/g).length;
-  if(x === o){
-    return true;
-  } else {
-    return false;
+  var countX=0
+  var countO=0
+  for(var i=0;i<str.length;i++){
+    if(str[i]==='x'){
+      countX++
+    }
+    else if(str[i]==='o'){
+      countO++
+    }
   }
+  if(countX===countO){
+    return true
+  }
+  else{return false}
     }
 console.log(xo('xoxoxo')); 
 console.log(xo('oxooxo'));
